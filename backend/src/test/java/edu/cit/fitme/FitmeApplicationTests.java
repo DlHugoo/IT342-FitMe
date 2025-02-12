6518405021824d5ffa24adba123af620e3768ff6
LAB14_USER ON WS29 <L14X08W29@citulab.net>
2025-02-12 09:07:33 +0800

    Added backend

---
+++ backend/src/test/java/edu/cit/fitme/FitmeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.fitme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
