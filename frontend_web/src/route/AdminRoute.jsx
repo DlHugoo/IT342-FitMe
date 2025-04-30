d7a9cb081c9504f82383521c756921f374e25c30
DlHugoo <hugo.davelawrence@gmail.com>
2025-04-30 09:56:24 +0800

    Protect Admin Routes

---
+++ frontend_web/src/route/AdminRoute.jsx
@@ -0,0 +1,16 @@
+// src/routes/AdminRoute.jsx
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+const AdminRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  const role = localStorage.getItem("role");
+
+  if (!token || role !== "admin") {
+    return <Navigate to="/login" replace />; // or /login if you prefer
+  }
+
+  return children;
+};
+
+export default AdminRoute;
